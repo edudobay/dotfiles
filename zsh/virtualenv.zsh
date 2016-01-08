c681839a0d8aff7b8dd48a2b270198f1dff9425d
Eduardo Dobay <edudobay@gmail.com>
2016-01-08 13:06:53 -0200

    zsh: search for virtualenvwrapper in other paths

--- zsh/virtualenv.zsh
+++ zsh/virtualenv.zsh
@@ -1,5 +1,20 @@
 # Python virtual environments
-if [[ -f /usr/bin/virtualenvwrapper.sh ]]; then
+
+# Common paths to virtualenvwrapper.sh in some distros
+# -- Lazy load if available
+virtualenv_paths=(
+   /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
+   /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+   /usr/bin/virtualenvwrapper.sh
+)
+
+# Use the first path that we find
+for venv_path in $virtualenv_paths; do
+if [[ -f $venv_path ]]; then
    VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
-   source /usr/bin/virtualenvwrapper.sh
+   source $venv_path
+   break
 fi
+done
+
+unset venv_path virtualenv_paths
