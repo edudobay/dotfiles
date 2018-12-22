2e81e40b650f8d5c78bd56b1f3caf973e261a274
Eduardo Dobay <edudobay@gmail.com>
2018-12-22 18:28:29 -0200

    zsh: update aliases and some default env vars

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,14 +3,24 @@ alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 
-alias ls="ls --color=auto --group-directories-first"
-alias l="ls -F"
+alias l='ls -F'
+alias ll='ls -lh'
+alias la='ls -lAh'
 
-alias tmux='TERM=xterm-256color tmux'
-alias mx='TERM=xterm-256color exec tmux'
+alias le='less -FNXmq'
+
+# alias tmux='TERM=xterm-256color tmux'
+# alias mx='TERM=xterm-256color exec tmux'
+
+alias ip4='ip -4 addr'
 
 # ls colors
-eval "$(dircolors)"
+if [[ $OSTYPE = darwin* ]]; then
+  alias ls='ls -G'
+else
+  eval "$(dircolors)"
+  alias ls='ls --color=auto --group-directories-first'
+fi
 
 function xrun()
 {
@@ -20,3 +30,20 @@ function xrun()
    disown "%$job_id"
 }
 
+alias cnf='command-not-found'
+
+function cdgit()
+{
+   local git_root
+   git_root=$(git rev-parse --show-toplevel 2>/dev/null) && \
+      cd $git_root
+}
+
+function CD()
+{
+    [[ $# -ne 1 ]] && {
+        echo 'Usage: CD <new-dir>'
+        return 1
+    }
+    mkdir -p $1 && cd $1
+}
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,5 +1,8 @@
 # Less: quit if one screen, avoid clearing, output ANSI colors
 export LESS="-F -X -R"
+# Set color for highlight (standout) mode
+export LESS_TERMCAP_so=$'\E[1;37;43m'
+export LESS_TERMCAP_se=$'\E[0;39;49m'
 
 [[ -n $TMUX ]] && export TERM=screen-256color
 
