0744eb2e8aba2211ca7436cbacb0ff64b1d394ae
Eduardo Dobay <edudobay@gmail.com>
2018-12-23 00:13:15 -0200

    zsh/android: recognize default SDK location on macOS

--- zsh/android.zsh
+++ zsh/android.zsh
@@ -1,6 +1,15 @@
-export ANDROID_HOME=$HOME/Android/Sdk
+for dir in \
+    $HOME/Library/Android/sdk \
+    $HOME/Android/Sdk;
+do
+  if [[ -d $dir ]]; then
+    export ANDROID_HOME=$dir
 
-path+=(
-  $ANDROID_HOME/platform-tools
-  $ANDROID_HOME/tools
-)
+    path+=(
+      $ANDROID_HOME/platform-tools
+      $ANDROID_HOME/tools
+    )
+
+    break
+  fi
+done
