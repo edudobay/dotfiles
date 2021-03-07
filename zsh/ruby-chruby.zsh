2a194708b26435cf28f51af0bd0845020ed31aec
Eduardo Dobay <edudobay@gmail.com>
2021-03-07 00:46:32 -0300

    zsh+ruby: Support chruby from multiple locations

--- zsh/ruby-chruby.zsh
+++ zsh/ruby-chruby.zsh
@@ -1,10 +1,20 @@
 [[ -o login ]] && return
 
 function _chruby_load() {
-    source ~/.local/share/chruby/chruby.sh
+  for root in \
+    /usr/local/opt/chruby \
+    ~/.local/
+  do
+    if [[ -f $root/share/chruby/chruby.sh ]]; then
+      source $root/share/chruby/chruby.sh
+      return
+    fi
+  done
+
+  echo "chruby not found" 1>&2
+  return 1
 }
 
 type chruby &>/dev/null || function chruby() {
-    _chruby_load
-    chruby "$@"
+  _chruby_load && chruby "$@"
 }
