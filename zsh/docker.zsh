87b9380a6b9911f8f5990056620631b8006a34ed
Eduardo Dobay <edudobay@gmail.com>
2021-03-07 00:57:43 -0300

    zsh: Add docker aliases

---
+++ zsh/docker.zsh
@@ -0,0 +1,15 @@
+docker-getip() {
+    local container=$1
+    docker inspect "$container" --format "{{range .NetworkSettings.Networks}}{{.IPAddress}}\n{{end}}"
+}
+
+dockermac() {
+    [[ $OSTYPE == darwin* ]] || {
+        echo 1>&2 "notice: this command should only be run under macOS"
+        return
+    }
+    open -j -g -a Docker.app
+}
+
+alias dc='docker-compose'
+alias dps="docker ps --format 'table {{.Names}}\t{{.ID}}\t{{.Status}}\t{{.Image}}'"
