6a3357ac2b037347abb3b4d813574053eecc1862
Eduardo Dobay <edudobay@gmail.com>
2022-01-09 13:18:51 -0300

    python: setup: Manually add console scripts, don't introspect
    This will probably be easier to maintain and to add new features.

--- python/dotfiles/scripts/__init__.py
+++ python/dotfiles/scripts/__init__.py
@@ -1,36 +1,21 @@
-import importlib
-import pkgutil
 import os
 import subprocess
-
-
-def find_available_console_scripts():
-    commands_path = __path__  # type: ignore  # mypy issue #1422
-
-    modules = list(pkgutil.iter_modules(commands_path))
-
-    def script_name(module_name):
-        return module_name.replace('_', '-')
-
-    def callable_name(module_name):
-        return f'dotfiles.scripts.{module_name}:main'
-
-    # TODO: Find a way to introspect modules without really importing them?
-    return [
-        f"{script_name(module.name)} = {callable_name(module.name)}"
-        for module in modules
-    ]
 
 
 def find_version() -> str:
     """
     Version will be date of last commit, in YYYYMMDD format.
     """
-    cmd = subprocess.run(
-        ['git', 'log', '-n1', '--format=%cs'],
-        cwd=os.path.dirname(__file__),
-        capture_output=True,
-        text=True,
-    )
+    try:
+        cmd = subprocess.run(
+            ['git', 'log', '-n1', '--format=%cs'],
+            cwd=os.path.dirname(__file__),
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+    except FileNotFoundError as e:
+        raise RuntimeError(f"Could not determine version from Git: {e}") from e
+
     date = cmd.stdout.strip()
     return date.replace('-', '')
--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup
-from dotfiles.scripts import find_available_console_scripts, find_version
+from dotfiles.scripts import find_version
 
 setup(
     version=find_version(),
-    entry_points={
-        'console_scripts': find_available_console_scripts(),
-    },
 )
