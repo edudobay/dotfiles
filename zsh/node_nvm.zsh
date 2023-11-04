50f1d6ddc1607a544c98efaac090938948f59f34
Eduardo Dobay <edudobay@gmail.com>
2023-11-04 10:04:00 -0300

    fix: node: don't source nvm stuff if it is not available

---
+++ zsh/node_nvm.zsh
@@ -0,0 +1,43 @@
+# Don't do anything if we cannot find the nvm executable.
+if ! type nvm &>/dev/null ]]; then
+    return
+fi
+
+if [[ -d /apps/nvm ]]; then
+    NVM_DIR="/apps/nvm"
+else
+    NVM_DIR="$HOME/.local/nvm"
+fi
+
+export NVM_DIR
+
+function _node_load() {
+    path prepend ~/.config/yarn/global/node_modules/.bin
+
+    ! whence -p nvm >/dev/null && \
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+}
+
+function nvm() {
+    _node_load && nvm "$@"
+}
+
+function node() {
+    ! whence -p node >/dev/null && _node_load
+    command node "$@"
+}
+
+function npm() {
+    ! whence -p npm >/dev/null && _node_load
+    command npm "$@"
+}
+
+function npx() {
+    ! whence -p npx >/dev/null && _node_load
+    command npx "$@"
+}
+
+function yarn() {
+    ! whence -p yarn >/dev/null && _node_load
+    command yarn "$@"
+}
