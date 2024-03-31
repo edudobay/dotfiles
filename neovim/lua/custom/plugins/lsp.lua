75b408ff0427ea44048a89a024c6994ce05dd42a
Eduardo Dobay <edudobay@gmail.com>
2024-03-30 23:00:28 -0300

    [neovim] fix key conflict

--- neovim/lua/custom/plugins/lsp.lua
+++ neovim/lua/custom/plugins/lsp.lua
@@ -3,7 +3,7 @@
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<space>zq', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
