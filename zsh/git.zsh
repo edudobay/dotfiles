e51afb45e656831d69e3884701993db819dee6d6
Eduardo Dobay <edudobay@gmail.com>
2026-04-13 20:43:36 -0300

    zsh: update git unalias list

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -4,7 +4,7 @@ alias gf="git fetch --prune"
 alias gs="git switch"
 
 # So many aliases!!!1 (From oh-my-zsh git plugin)
-unalias git-svn-dcommit-push glum gmom gmum g gam gama gamc gams gamscp gap gapa gapt gau gav gbs gbsb gbsg gbsr gbss 'gcan!' 'gcans!' gcd gcf gcl gcmsg 'gcn!' gcount gfg gga ghh gignore gignored gk gke gpristine gpu gpv gr gra grev grh grhh grm grmc grmv groh grrm grs grset grss grt gru grup grv gsb gsd gsh gsi gsps gsr gss gsta gstaa gstall gstc gstd gstl gstp gsts gstu gsu gts gtv gunignore gunwip gup gupa gupav gupv gwch gwip
+unalias -m git-svn-dcommit-push glum gmom gmum g gam gama gamc gams gamscp gap gapa gapt gau gav gbs gbsb gbsg gbsr gbss 'gcan!' 'gcans!' gcd gcf gcl gcmsg 'gcn!' gcount gfg gga ghh gignore gignored gk gke gpristine gpu gpv gr gra grev grh grhh grm grmc grmv groh grrm grs grset grss grt gru grup grv gsb gsd gsh gsi gsps gsr gss gsta gstaa gstall gstc gstd gstl gstp gsts gstu gsu gts gtv gunignore gunwip gwch gwip || true
 
 
 git-delete-merged-branches() {
