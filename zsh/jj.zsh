b7e0f55a524381e25802548f667a5efd357700fb
Eduardo Dobay <edudobay@gmail.com>
2026-04-28 23:25:00 -0300

    zsh/jj: add complete action (^X j)

--- zsh/jj.zsh
+++ zsh/jj.zsh
@@ -1,3 +1,21 @@
 # Jujutsu (VCS) - https://docs.jj-vcs.dev/latest/
 
-alias jju='lazyjj'
+# adapted from https://lobste.rs/s/exlogg/jjj
+_fzf_insert_jj_commit() {
+  local selected rev
+
+  selected=$(
+    jj log -r 'all()' -T builtin_log_oneline --color=always \
+      | fzf \
+          --min-height=15 \
+          --cycle \
+          --ansi \
+          --prompt "jj rev> "
+  )
+
+  rev=$(echo "$selected" | awk '{for(i=1;i<=NF;i++) if(length($i)>=7){print $i; exit}}')
+
+  LBUFFER="${LBUFFER}${rev}"
+}
+zle -N _fzf_insert_jj_commit
+bindkey '^Xj' _fzf_insert_jj_commit
