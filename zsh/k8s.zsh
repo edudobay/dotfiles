f14a9314e6dccc336ba8d4a9014c93554d505856
Eduardo Dobay <edudobay@gmail.com>
2026-06-10 15:33:06 -0300

    zsh/k8cfg: add prefix/exact search, and support running command directly

--- zsh/k8s.zsh
+++ zsh/k8s.zsh
@@ -1,7 +1,33 @@
 k8cfg() {
-    config_file=$(ls ~/.kube/*config | fzf)
-    [[ -n $config_file ]] || return 1
+    local kubedir=~/.kube config_file _KUBECONFIG
+    if [[ $1 == :* ]]; then
+        local search_prefix=${1#:} config_files
+        shift
+        if [[ -f $kubedir/${search_prefix}.config ]]; then
+            _KUBECONFIG=$kubedir/${search_prefix}.config  # exact match
+        else
+            config_files=($(ls $kubedir/${search_prefix}*config))
+            if [[ ${#config_files} -eq 1 ]]; then
+                _KUBECONFIG=${config_files[1]}
+            else
+                echo "${#config_files} matches found for '${search_prefix}'; expected exactly 1" 1>&2
+                return 1
+            fi
+        fi
+    else
+        _KUBECONFIG=$(ls $kubedir/*config | fzf)
+        if [[ ! -n $_KUBECONFIG ]]; then
+            echo "no config files found in ~/.kube" 1>&2
+            return 1
+        fi
+    fi
 
-    export KUBECONFIG=$config_file
-    echo "Using KUBECONFIG=$KUBECONFIG"
+    echo "Using KUBECONFIG=$_KUBECONFIG"
+
+    if [[ $# -gt 0 ]]; then
+        KUBECONFIG=$_KUBECONFIG "$@"
+        return
+    fi
+
+    export KUBECONFIG=$_KUBECONFIG
 }
